816883c3b3566a3e5ee092344dd5aabdd2c1144a
DenZv95 <den.zv95@yandex.ru>
2022-11-02 22:10:33 +0900

    добавлен обработчик ошибок

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 
 import { connectDB } from "./config/db.js";
 
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
 import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
@@ -18,6 +20,8 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 app.use("/api/users", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server run on ${PORT}`));
